9bc498c54008f7adcec98eecbaf73f018ee22c4a
Julissa Gomez <JULISSAGOMEZ1927@GMAIL.COM>
2026-04-17 14:17:34 -0700

    Update FireInit.m
    Co-Authored-By: isaaksmith05 <270815389+isaaksmith05@users.noreply.github.com>
    Co-Authored-By: svaz3829 <270817552+svaz3829@users.noreply.github.com>

--- FireInit.m
+++ FireInit.m
@@ -1,16 +1,28 @@
-function [outputArg1,outputArg2] = FireInit(inputArg1,inputArg2)
-%FIREINIT Summary of this function goes here
-%   Detailed explanation goes here
-arguments (Input)
-    inputArg1
-    inputArg2
-end
+function fire = FireInit(param)
+%FIREINIT will initiate the fire. Starting from the grid size. We'll have a
+%fire_start that will initial start with 3 cells of fire randomally
+%generated into the grid. These 3 cells will be populated with a randomly
+%generated intensity value between 0 and 1.
 
-arguments (Output)
-    outputArg1
-    outputArg2
-end
+    % Get grid size
+    rows = param.grid_size(1)
+    cols = param.grid_size(2)
+    
+    % Create empty grid
+    fire.intensity = zeros(rows, cols)
+    
+    % Number of initial fires
+    fire_start = 3
+    
+    for ii = 1:fire_start
+        
+        % Pick random location in grid
+        r = randi(rows)
+        c = randi(cols)
+        
+        % Assign random intensity between 0 and 1
+        fire.intensity(r, c) = rand()
+    
+    end
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
 end
