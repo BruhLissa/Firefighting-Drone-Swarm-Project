c81bf5e30423f56dee2557270b97526552ae2e61
svaz3829 <svaz3829@giant.cos.edu>
2026-04-20 14:03:31 -0700

    Add files via upload

---
+++ FirstTest/assignTarget.m
@@ -0,0 +1,22 @@
+function drone = assignTarget(drone, fire)
+% assignTarget selects the highest intensity fire cell
+% Inputs:
+%   drone - single drone struct
+%   fire  - fire struct with intensity matrix
+% Output:
+%   drone - updated drone with target
+
+    %find max fire intensity
+    [maxVal, index] = max(fire.intensity(:));
+
+    %if no fire exists
+    if maxVal == 0
+        drone.target = [];
+        return;
+    end
+
+    %convert index to (row, col)
+    [row, col] = ind2sub(size(fire.intensity), index);
+    %assign target
+    drone.target = [row, col];
+end
